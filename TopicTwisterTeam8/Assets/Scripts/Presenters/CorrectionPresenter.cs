ce27ba334178474dd6eba0ab3a2adf0327feebbe
tkz00 <theokatz@gmail.com>
2022-06-09 18:05:19 -0300

    CorrectionView & CorrectionPresenter

---
+++ TopicTwisterTeam8/Assets/Scripts/Presenters/CorrectionPresenter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopicTwister.Assets.Scripts.Models;
+
+namespace Assets.Scripts.Presenters
+{
+    public class CorrectionPresenter
+    {
+        ICorrectionView _view;
+
+        public CorrectionPresenter(ICorrectionView view)
+        {
+            _view = view;
+        }
+
+        public void GetCorrections(string[] roundCategories, string[] answers)
+        {
+
+        }
+    }
+}
---
+++ TopicTwisterTeam8/Assets/Scripts/Views/CorrectionView.cs
@@ -0,0 +1,43 @@
+using Assets.Scripts.Presenters;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Team8.TopicTwister
+{
+    public class CorrectionView : MonoBehaviour, ICorrectionView
+    {
+        public event Action OnNextTurnClick;
+
+        [SerializeField]
+        private TMP_Text[] categories;
+
+        [SerializeField]
+        private TMP_Text[] playerAnswers;
+
+        [SerializeField]
+        private Image[] playerResults;
+
+        [SerializeField]
+        private TMP_Text roundLetter;
+
+        [SerializeField]
+        private Button nextTurnButton;
+
+        CorrectionPresenter _presenter;
+
+
+        private void Start()
+        {
+            _presenter = new CorrectionPresenter(this);
+        }
+
+        public void ShowCorrections()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
