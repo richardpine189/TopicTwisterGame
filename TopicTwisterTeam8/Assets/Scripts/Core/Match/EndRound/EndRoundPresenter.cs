70bd6659ad12322a89f64ce3cba5a07f87ba4821
tkz00 <theokatz@gmail.com>
2022-10-05 20:07:12 -0300

    Get match id in endRoundPresenter

--- TopicTwisterTeam8/Assets/Scripts/Core/Match/EndRound/EndRoundPresenter.cs
+++ TopicTwisterTeam8/Assets/Scripts/Core/Match/EndRound/EndRoundPresenter.cs
@@ -6,13 +6,14 @@ public class EndRoundPresenter
 {
     private readonly IEndRoundView _endRoundView;
     private readonly IGetRoundResult _getRoundResultUseCase;
+    private IGetMatchId _getMachId;
     private IGetMatchData _getMatchData;
-    private int _matchId;
     private const int LAST_ROUND = 2;
 
-    public EndRoundPresenter(IEndRoundView endRoundView, IGetRoundResult getRoundResultUseCase, IGetMatchData getMatchData)
+    public EndRoundPresenter(IEndRoundView endRoundView, IGetRoundResult getRoundResultUseCase, IGetMatchData getMatchData, IGetMatchId getMatchId)
     {
         _getRoundResultUseCase = getRoundResultUseCase;
+        _getMachId = getMatchId;
         _getMatchData = getMatchData;
         _endRoundView = endRoundView;
         _endRoundView.OnSetRoundResults += RequestRoundResult;
@@ -26,6 +27,7 @@ public class EndRoundPresenter
     private async void RequestRoundResult()
     {
         int roundNumber = _getMatchData.GetRoundNumber();
+        int _matchId = _getMachId.Invoke();
         MatchResultsDTO matchResultsDTO = await _getRoundResultUseCase.Execute(_matchId);
 
         if (roundNumber == LAST_ROUND && matchResultsDTO.matchStatus != WinnerStatus.Unassigned)
